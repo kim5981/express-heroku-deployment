a47de78b613ab4f2673185b674ada32b46cb379b
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-12 15:07:55 -0700

    randomom idx

--- api/server.js
+++ api/server.js
@@ -1,14 +1,20 @@
 const express = require("express")
 const theories = require("./conspiracyTheories")
+
 const server = express()
 
 server.get("/", (req, res) => {
     res.send("<h1>kim was here</h1>")
 })
 
-server.get("/theories", (req, res) => {
+server.get("/node-env", (req, res) => {
+    res.json({ NODE_ENV: process.env.NODE_ENV })
+})
+
+// get a random theory
+server.get("/theory", (req, res) => {
     let randIdx = Math.floor(Math.random() * theories.length)
-    res.send(`<h1> ${theories(randIdx)} </h1>`)
+    res.send(`<h1> ${theories[randIdx]} </h1>`)
 })
 
 module.exports = server
